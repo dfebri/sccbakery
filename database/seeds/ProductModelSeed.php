b846cc0e044908bbd04d1f87460bfddd8ee979f8
febri <dwifebrimurcahyo@gmail.com>
2025-06-18 11:00:17 +0700

    add seeder productmodelseed

--- database/seeds/ProductModelSeed.php
+++ database/seeds/ProductModelSeed.php
@@ -15,32 +15,82 @@ Class ProductModelSeed extends Seeder
                 [
                     'category_id' => 12,
                     'brand_id' => 36,
-                    'name' => 'SUN6',
+                    'name' => 'EASY6 BIANCO',
                     'subtitle' => '',
                     'description' => '
-                    <div>
-                        Weight: &nbsp;<br />
-                        Power : &nbsp;<br />
-                        Max temperature       : &nbsp;<br />
-                        Dimensions (LxPxA)    : &nbsp;<br />
-                        Electric Power Supply : &nbsp;<br />
-                        <div>&nbsp;</div>
-                        <div>Accessories : </div>
-                        <div>&nbsp;
-                    </div>
-                    <p>&nbsp;</p>',
+                    description',
                     'video' => '',
-                    'permalink' => 'sun6',
-                    'publish' => 1,
+                    'permalink' => 'easy-6-bianco',
+                    'publish' =>1,
                     'order_category' =>0,
                     'order_brand' =>0,
                     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                     'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                    'img' => 'sunmix lavender.jpg',
-                    'meta_title' => 'PIZZA OVEN',
-                    'meta_keyword' => 'Pizaa Oven, Mesin Bakery, Mesin Bakery Jakarta',
-                    'meta_description' => 'Deck Oven Mesin Bakery'
-                    
+                    'img' => 'EASY6_bianco.jpg',
+                    'meta_title' => 'easy6 bianco',
+                    'meta_keyword' => 'easy6, easy6 bianco, spiral mixer, 
+                    spiral mixer jakarta, mesin bakery, mesin bakery jakarta',
+                    'meta_description' => 'spiral mixer easy6 bianco' 
+                ],     
+                [
+                    'category_id' => 12,
+                    'brand_id' => 36,
+                    'name' => 'EASY10 NERO',
+                    'subtitle' => '',
+                    'description' => '
+                    description',
+                    'video' => '',
+                    'permalink' => 'easy-10-nero',
+                    'publish' =>1,
+                    'order_category' =>0,
+                    'order_brand' =>0,
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'img' => 'EASY10_nero.jpg',
+                    'meta_title' => 'easy10 nero',
+                    'meta_keyword' => 'easy10, easy10 nero, spiral mixer, 
+                    spiral mixer jakarta, mesin bakery, mesin bakery jakarta',
+                    'meta_description' => 'spiral mixer easy6 nero' 
+                ], 
+                [
+                    'category_id' => 12,
+                    'brand_id' => 36,
+                    'name' => 'EASY15 ROSSO',
+                    'subtitle' => '',
+                    'description' => '
+                    description',
+                    'video' => '',
+                    'permalink' => 'easy-15-rosso',
+                    'publish' =>1,
+                    'order_category' =>0,
+                    'order_brand' =>0,
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'img' => 'EASY15_rosso.jpg',
+                    'meta_title' => 'easy15 rosso',
+                    'meta_keyword' => 'easy15, easy15 rosso, spiral mixer, 
+                    spiral mixer jakarta, mesin bakery, mesin bakery jakarta',
+                    'meta_description' => 'spiral mixer easy15 rosso' 
+                ],
+                [
+                    'category_id' => 12,
+                    'brand_id' => 36,
+                    'name' => 'EASY20 BIANCO',
+                    'subtitle' => '',
+                    'description' => '
+                    description',
+                    'video' => '',
+                    'permalink' => 'easy-20-bianco',
+                    'publish' =>1,
+                    'order_category' =>0,
+                    'order_brand' =>0,
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'img' => 'EASY20_bianco.jpg',
+                    'meta_title' => 'easy20 bianco',
+                    'meta_keyword' => 'easy20, easy20 bianco, spiral mixer, 
+                    spiral mixer jakarta, mesin bakery, mesin bakery jakarta',
+                    'meta_description' => 'spiral mixer easy6 bianco' 
                 ]
             ]
         );
