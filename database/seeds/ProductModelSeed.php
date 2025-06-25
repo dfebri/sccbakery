28b8da4eb4e8521234c7572504c0b05c3b50ea60
febri <dwifebrimurcahyo@gmail.com>
2025-06-25 09:56:44 +0700

    Modified ProductModelSeed for Bernadi Product

--- database/seeds/ProductModelSeed.php
+++ database/seeds/ProductModelSeed.php
@@ -14,84 +14,44 @@ Class ProductModelSeed extends Seeder
             [
                 [
                     'category_id' => 12,
-                    'brand_id' => 36,
-                    'name' => 'EASY6 BIANCO',
+                    'brand_id' => 37,
+                    'name' => 'MISS BAKER PRO',
                     'subtitle' => '',
                     'description' => '
                     description',
                     'video' => '',
-                    'permalink' => 'easy-6-bianco',
+                    'permalink' => 'miss-baker-pro',
                     'publish' =>1,
                     'order_category' =>0,
                     'order_brand' =>0,
                     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                     'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                    'img' => 'EASY6_bianco.jpg',
-                    'meta_title' => 'easy6 bianco',
-                    'meta_keyword' => 'easy6, easy6 bianco, spiral mixer, 
+                    'img' => 'MISS-BAKER-PRO.jpg',
+                    'meta_title' => 'miss baker pro',
+                    'meta_keyword' => 'bernadi, miss baker pro, spiral mixer, 
                     spiral mixer jakarta, mesin bakery, mesin bakery jakarta',
-                    'meta_description' => 'spiral mixer easy6 bianco' 
+                    'meta_description' => 'spiral mixer bernadi miss baker pro' 
                 ],     
                 [
                     'category_id' => 12,
-                    'brand_id' => 36,
-                    'name' => 'EASY10 NERO',
+                    'brand_id' => 37,
+                    'name' => 'MISS BAKER PRO XL',
                     'subtitle' => '',
                     'description' => '
                     description',
                     'video' => '',
-                    'permalink' => 'easy-10-nero',
+                    'permalink' => 'miss-baker-pro-xl',
                     'publish' =>1,
                     'order_category' =>0,
                     'order_brand' =>0,
                     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                     'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                    'img' => 'EASY10_nero.jpg',
-                    'meta_title' => 'easy10 nero',
-                    'meta_keyword' => 'easy10, easy10 nero, spiral mixer, 
+                    'img' => 'MISS-BAKER-PRO-XL.jpg',
+                    'meta_title' => 'miss baker pro xl',
+                    'meta_keyword' => 'bernadi, miss baker pro xl, spiral mixer, 
                     spiral mixer jakarta, mesin bakery, mesin bakery jakarta',
-                    'meta_description' => 'spiral mixer easy6 nero' 
-                ], 
-                [
-                    'category_id' => 12,
-                    'brand_id' => 36,
-                    'name' => 'EASY15 ROSSO',
-                    'subtitle' => '',
-                    'description' => '
-                    description',
-                    'video' => '',
-                    'permalink' => 'easy-15-rosso',
-                    'publish' =>1,
-                    'order_category' =>0,
-                    'order_brand' =>0,
-                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                    'img' => 'EASY15_rosso.jpg',
-                    'meta_title' => 'easy15 rosso',
-                    'meta_keyword' => 'easy15, easy15 rosso, spiral mixer, 
-                    spiral mixer jakarta, mesin bakery, mesin bakery jakarta',
-                    'meta_description' => 'spiral mixer easy15 rosso' 
-                ],
-                [
-                    'category_id' => 12,
-                    'brand_id' => 36,
-                    'name' => 'EASY20 BIANCO',
-                    'subtitle' => '',
-                    'description' => '
-                    description',
-                    'video' => '',
-                    'permalink' => 'easy-20-bianco',
-                    'publish' =>1,
-                    'order_category' =>0,
-                    'order_brand' =>0,
-                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                    'img' => 'EASY20_bianco.jpg',
-                    'meta_title' => 'easy20 bianco',
-                    'meta_keyword' => 'easy20, easy20 bianco, spiral mixer, 
-                    spiral mixer jakarta, mesin bakery, mesin bakery jakarta',
-                    'meta_description' => 'spiral mixer easy6 bianco' 
-                ]
+                    'meta_description' => 'spiral mixer bernadi miss baker pro xl' 
+                ] 
             ]
         );
     }
