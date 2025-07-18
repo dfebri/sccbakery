245403d7e173edd2b478929ec3908fd2e9903a20
febri <dwifebrimurcahyo@gmail.com>
2025-07-18 08:44:20 +0700

    update ProductModel

--- app/Admin/Models/ProductModel.php
+++ app/Admin/Models/ProductModel.php
@@ -1,36 +1,60 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Kim
- * Date: 6/23/2015
- * Time: 2:52 PM
- */
-namespace sccbakery\Admin\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class ProductModel extends Model {
-    protected $table    = 'product';
-
-    public function category() {
-        return $this->belongsTo('sccbakery\Admin\Models\CategoryModel','category_id', 'id');
-    }
-
-    public function brand() {
-        return $this->belongsTo('sccbakery\Admin\Models\BrandModel', 'brand_id', 'id');
-    }
-
-    public function images(){
-        return $this->hasMany('sccbakery\Admin\Models\ProductImageModel', 'product_id')
-            ->orderBy('as_default', 'desc')
-            ->orderBy('id', 'asc');
-    }
-
-    public function image($product_id){
-        $img = $this->images->find($product_id);
-
-        return $img->orderBy('as_default', 'desc')
-            ->orderBy('id', 'asc')
-            ->first();
-    }
+<?php
+
+/**
+
+ * Created by PhpStorm.
+
+ * User: Kim
+
+ * Date: 6/23/2015
+
+ * Time: 2:52 PM
+
+ */
+
+namespace sccbakery\Admin\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductModel extends Model {
+
+    protected $table    = 'product';
+
+
+
+    public function category() {
+
+        return $this->belongsTo('sccbakery\Admin\Models\CategoryModel','category_id', 'id');
+
+    }
+
+    public function brand() {
+
+        return $this->belongsTo('sccbakery\Admin\Models\BrandModel', 'brand_id', 'id');
+
+    }
+
+    public function images(){
+
+        return $this->hasMany('sccbakery\Admin\Models\ProductImageModel', 'product_id')
+
+            ->orderBy('as_default', 'desc')
+
+            ->orderBy('id', 'asc');
+
+    }
+
+    public function image($product_id){
+
+        $img = $this->images->find($product_id);
+
+
+
+        return $img->orderBy('as_default', 'desc')
+
+            ->orderBy('id', 'asc')
+
+            ->first();
+
+    }
+
 }
